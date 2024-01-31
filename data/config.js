bd3845477d0c1fd8c6a800b89e1fa9d48b07850e
lei.tian <whereismoney@qq.com>
2024-01-31 22:47:14 +0800

    first commit

---
+++ data/config.js
@@ -0,0 +1,21 @@
+// 抽奖人员
+var member = [
+    '张三', '李四', '张娟', '孙军', '林艳', '马秀英', '刘秀英', '朱涛',
+    '郭磊', '黄艳', '何勇', '高杰', '胡磊', '吴磊', '郭强', '高洋',
+    '黄丽', '徐勇', '赵超', '林涛', '朱勇', '杨军'
+];
+
+// 奖品
+var prizes = [
+    {name: 'TBox 茶合', prize: "三等奖", level: 3, "url": "data/prize/a11.png", person: ""},
+    {name: "九阳电饭煲", prize: "三等奖", level: 3, "url": "data/prize/a10.png", person: ""},
+    {name: "小米挂烫机", prize: "三等奖", level: 3, "url": "data/prize/a9.png", person: ""},
+    {name: "小米加湿器", prize: "三等奖", level: 3, "url": "data/prize/a8.png", person: ""},
+    {name: "小米手环", prize: "三等奖", level: 3, "url": "data/prize/a7.png", person: ""},
+    {name: "小米电动牙刷", prize: "三等奖", level: 3, "url": "data/prize/a6.png", person: ""},
+    {name: "小米电热杯", prize: "三等奖", level: 3, "url": "data/prize/a5.png", person: ""},
+    {name: "拍立得", prize: "二等奖", level: 2, "url": "data/prize/a4.png", person: ""},
+    {name: "料理机", prize: "二等奖", level: 2, "url": "data/prize/a3.png", person: ""},
+    {name: "米家清洁套装", prize: "二等奖", level: 2, "url": "data/prize/a2.png", person: ""},
+    {name: "iPhone 15", prize: "一等奖", level: 1, "url": "data/prize/a1.png", person: ""}
+];
